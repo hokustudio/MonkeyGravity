da5ceee6f24c651ae42d88229277037e20859cdd
aimzeter <mzrausyanfikri@gmail.com>
2015-03-22 16:28:11 +0700

    Menu
    Tes

---
+++ Assets/Resources/Scripts/Menu/PlayScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayScript : MonoBehaviour {
+	void Start(){
+		Debug.Log(111);
+	}
+
+	public void LoadScene(){
+		Debug.Log(222);
+		Application.LoadLevel(1);
+		Debug.Log(333);
+	}
+}
